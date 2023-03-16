a3a6ac08a82c0a24440dbb72f885e1a97c7cd957
silviosalza <silviofilipposalza@gmail.com>
2023-03-16 17:36:42 +0100

    palindrome

--- js/script.js
+++ js/script.js
@@ -2,14 +2,35 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma.
 
+const wordArray = []
 const userWord = prompt("Dimmi una parola a casissimo")
+console.log(userWord);
+
+const result = palindromo(userWord , wordArray)
+console.log(wordArray);
+
+
+
+
+
+
 
 
 
 
 //! Funzioni
 
-function palindromo ()
+function palindromo (word , letterArray){
+
+    letterArray = []
+    let i = 0
+    while (i < word.lenght){
+        const letter = word.charAt(i)
+        i++
+        letterArray.push(letter)
+    }
+    return letterArray
+}
 
 
 // **Pari e Dispari**
@@ -20,29 +41,29 @@ function palindromo ()
 // Dichiariamo chi ha vinto.
 
 
-const userChoice = prompt("Facciamo un gioco...pari o dispari?")
-console.log(userChoice);
-const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"))
-console.log(userNumber);
+// const userChoice = prompt("Facciamo un gioco...pari o dispari?")
+// console.log(userChoice);
+// const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"))
+// console.log(userNumber);
 
-if (!userChoice.includes("pari" , "dispari")){
-    alert("Scegli pari o dispari ho detto...")
+// if (!userChoice.includes("pari" , "dispari")){
+//     alert("Scegli pari o dispari ho detto...")
 
-} else if (userNumber <= 0 || userNumber >= 6) {
-    alert("Ti sembra un numero tra 1 e 5?")
-} else {
+// } else if (userNumber <= 0 || userNumber >= 6) {
+//     alert("Ti sembra un numero tra 1 e 5?")
+// } else {
 
-    const CPUnumber = getRndInteger(1,5)
-    console.log(CPUnumber);
-    const result = ODDorEVEN(userNumber, CPUnumber)
-    console.log(result);
+//     const CPUnumber = getRndInteger(1,5)
+//     console.log(CPUnumber);
+//     const result = ODDorEVEN(userNumber, CPUnumber)
+//     console.log(result);
 
-    if (userChoice === result){
-        alert("Vivrai ancora per un altro refresh...")
-    } else {
-        alert("Mi dispiace, hai perso....knock knock...")
-    }
-} 
+//     if (userChoice === result){
+//         alert("Vivrai ancora per un altro refresh...")
+//     } else {
+//         alert("Mi dispiace, hai perso....knock knock...")
+//     }
+// } 
 
 
 //! Funzioni
